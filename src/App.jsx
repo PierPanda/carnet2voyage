b7c28056afe8426425e712de2476c846dac5fc65
Pierre Bermudez <bermudezpierre@gmail.com>
2025-10-29 10:59:14 +0100

    chore: integrate Tailwind CSS into the project
    - Added Tailwind CSS and its Vite plugin to dependencies.
    - Removed custom CSS styles and replaced them with Tailwind utility classes in App component.
    - Updated App.css and index.css to import Tailwind CSS.
    - Configured Vite to use Tailwind CSS plugin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="home-centered">home</div>;
+  return (
+    <div className="flex items-center justify-center h-screen text-center text-3xl font-bold">
+      home
+    </div>
+  );
 }
 
 export default App;
